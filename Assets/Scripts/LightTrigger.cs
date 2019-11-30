22b71452f56e5a5df3b7f325bcf6d0095bf4bef9
xkyl-yhw <xkylhh@163.com>
2019-11-30 21:14:41 +0800

    yhw 2019:11:30 21:14

--- Assets/Scripts/LightTrigger.cs
+++ Assets/Scripts/LightTrigger.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class LightTrigger : MonoBehaviour
 {
     public GameObject gear;
-    public float speed;
     public bool triggered = false;
-    public float offsetz = 0.5f;
 
     private Vector3 prePos;
 
@@ -20,11 +18,11 @@ public class LightTrigger : MonoBehaviour
     {
         if (triggered)
         {
-            gear.transform.position = Vector3.Lerp(gear.transform.position, prePos + Vector3.back * offsetz, Time.deltaTime * speed);
+            gear.GetComponent<BoxCollider2D>().enabled = true;
         }
         else
         {
-            gear.transform.position = Vector3.Lerp(gear.transform.position, prePos, Time.deltaTime * speed);
+            gear.GetComponent<BoxCollider2D>().enabled = false;
         }
     }
 
